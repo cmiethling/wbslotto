35cfb40683a71d3c75e76950feff7d3a0e66bfd5
cmiethli <christian.miethling86@gmail.com>
2020-05-10 14:58:36 +0200

    users2id ("user_id") zu users2 + getter,setter
    -wo map auf Users2-Objekt ist:
    UserRoles

--- src/main/java/de/wbstraining/lotto/persistence/model/UserRoles.java
+++ src/main/java/de/wbstraining/lotto/persistence/model/UserRoles.java
@@ -28,103 +28,107 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "user_roles")
 @NamedQueries({
-    @NamedQuery(name = "UserRoles.findAll", query = "SELECT ur FROM UserRoles ur"),
-    @NamedQuery(name = "UserRoles.findByUserRoleId", query = "SELECT ur FROM UserRoles ur WHERE ur.userRolesId = :userRolesId"),
-    @NamedQuery(name = "UserRoles.findByRole", query = "SELECT ur FROM UserRoles ur WHERE ur.role = :role"),
-    @NamedQuery(name = "UserRoles.findByUserName", query = "SELECT ur FROM UserRoles ur WHERE ur.userName = :userName")})
+		@NamedQuery(name = "UserRoles.findAll", query = "SELECT ur FROM UserRoles ur"),
+		@NamedQuery(name = "UserRoles.findByUserRoleId", query = "SELECT ur FROM UserRoles ur WHERE ur.userRolesId = :userRolesId"),
+		@NamedQuery(name = "UserRoles.findByRole", query = "SELECT ur FROM UserRoles ur WHERE ur.role = :role"),
+		@NamedQuery(name = "UserRoles.findByUserName", query = "SELECT ur FROM UserRoles ur WHERE ur.userName = :userName") })
 public class UserRoles implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)    
-    @Basic(optional = false)
-    @Column(name = "user_rolesid")
-    private Long userRolesId;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 20)
-    @Column(name = "role")
-    private String role;
-    @Size(max = 200)
-    @Column(name = "username")
-    private String userName;
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-    @ManyToOne(optional = false)
-    private Users2 userId;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "user_rolesid")
+	private Long userRolesId;
+	@Basic(optional = false)
+	@NotNull
+	@Size(min = 1, max = 20)
+	@Column(name = "role")
+	private String role;
+	@Size(max = 200)
+	@Column(name = "username")
+	private String userName;
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	@ManyToOne(optional = false)
+	private Users2 users2;
 
-    public UserRoles() {
-    }
+	public UserRoles() {
+	}
 
-    public UserRoles(Long userRolesId) {
-        this.userRolesId = userRolesId;
-    }
+	public UserRoles(Long userRolesId) {
+		this.userRolesId = userRolesId;
+	}
 
-    public UserRoles(Long userRolesId, String role) {
-        this.userRolesId = userRolesId;
-        this.role = role;
-    }
+	public UserRoles(Long userRolesId, String role) {
+		this.userRolesId = userRolesId;
+		this.role = role;
+	}
 
-    public UserRoles(Long userRolesId, String role, String userName) {
-        this.userRolesId = userRolesId;
-        this.role = role;
-        this.userName  = userName;
-    }
+	public UserRoles(Long userRolesId, String role, String userName) {
+		this.userRolesId = userRolesId;
+		this.role = role;
+		this.userName = userName;
+	}
 
-    public Long getUserRolesId() {
-        return userRolesId;
-    }
+	public Long getUserRolesId() {
+		return userRolesId;
+	}
 
-    public void setUserRolesId(Long userRolesId) {
-        this.userRolesId = userRolesId;
-    }
+	public void setUserRolesId(Long userRolesId) {
+		this.userRolesId = userRolesId;
+	}
 
-    public String getRole() {
-        return role;
-    }
+	public String getRole() {
+		return role;
+	}
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+	public void setRole(String role) {
+		this.role = role;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public Users2 getUserid() {
-        return userId;
-    }
+	public Users2 getUsers2() {
+		return users2;
+	}
 
-    public void setUserid(Users2 userId) {
-        this.userId = userId;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (userRolesId != null ? userRolesId.hashCode() : 0);
-        return hash;
-    }
+	public void setUsers2(Users2 userId) {
+		this.users2 = userId;
+	}
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof UserRoles)) {
-            return false;
-        }
-        UserRoles other = (UserRoles) object;
-        if ((this.userRolesId == null && other.userRolesId != null) || (this.userRolesId != null && !this.userRolesId.equals(other.userRolesId))) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (userRolesId != null ? userRolesId.hashCode() : 0);
+		return hash;
+	}
 
-    @Override
-    public String toString() {
-        return "wbs.corejpa.persistence.UserRoles[ userRolesId=" + userRolesId + " ]";
-    }
-    
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not
+		// set
+		if (!(object instanceof UserRoles)) {
+			return false;
+		}
+		UserRoles other = (UserRoles) object;
+		if ((this.userRolesId == null && other.userRolesId != null)
+				|| (this.userRolesId != null
+						&& !this.userRolesId.equals(other.userRolesId))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "wbs.corejpa.persistence.UserRoles[ userRolesId=" + userRolesId
+				+ " ]";
+	}
+
 }
--- src/main/java/de/wbstraining/lotto/persistence/model/Users2.java
+++ src/main/java/de/wbstraining/lotto/persistence/model/Users2.java
@@ -1,13 +1,16 @@
 /*
- https://github.com/join * To change this license header, choose License Headers in Project Properties.
+ * https://github.com/join * To change this license header, choose License
+ * Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package de.wbstraining.lotto.persistence.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,6 +19,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -28,71 +32,71 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "users2")
 @NamedQueries({
-    @NamedQuery(name = "Users2.findAll", query = "SELECT u FROM Users2 u"),
-    @NamedQuery(name = "Users2.findByUserId", query = "SELECT u FROM Users2 u WHERE u.userId = :userId"),    
-    @NamedQuery(name = "Users2.findByUsername", query = "SELECT u FROM Users2 u WHERE u.username = :username"),
-    @NamedQuery(name = "Users2.findByPassword", query = "SELECT u FROM Users2 u WHERE u.password = :password")})
+		@NamedQuery(name = "Users2.findAll", query = "SELECT u FROM Users2 u"),
+		@NamedQuery(name = "Users2.findByUserId", query = "SELECT u FROM Users2 u WHERE u.userId = :userId"),
+		@NamedQuery(name = "Users2.findByUsername", query = "SELECT u FROM Users2 u WHERE u.username = :username"),
+		@NamedQuery(name = "Users2.findByPassword", query = "SELECT u FROM Users2 u WHERE u.password = :password") })
 public class Users2 implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "user_id")
-    private Integer userId;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 10)
-    @Column(name = "username")
-    private String username;
-    @NotNull
-    @Size(min = 1, max = 64)
-    @Column(name = "password")
-    private String password;
-    
-//	@OneToMany(cascade = CascadeType.ALL, mappedBy = "userId")
-//	private List<UserRoles> userRolesList;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "user_id")
+	private Integer userId;
+	@Basic(optional = false)
+	@NotNull
+	@Size(min = 1, max = 10)
+	@Column(name = "username")
+	private String username;
+	@NotNull
+	@Size(min = 1, max = 64)
+	@Column(name = "password")
+	private String password;
 
-    @JoinColumn(name = "kundeid", referencedColumnName = "kundeid")
-    @OneToOne
-    private Kunde kunde; 
-    
-    public Users2() {
-    }
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "users2")
+	private List<UserRoles> userRolesList;
 
-    public Users2(Integer userId) {
-        this.userId = userId;
-    }
+	@JoinColumn(name = "kundeid", referencedColumnName = "kundeid")
+	@OneToOne
+	private Kunde kunde;
 
-    public Users2(Integer userId, String username, String password) {
-        this.userId = userId;
-        this.username = username;
-        this.password = password;
-    }
-    
-    public String getUsername() {
-        return username;
-    }
+	public Users2() {
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public Users2(Integer userId) {
+		this.userId = userId;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public Users2(Integer userId, String username, String password) {
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public Kunde getKunde() {
-        return kunde;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setKunde(Kunde kundeid) {
-        this.kunde = kundeid;
-    } 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kundeid) {
+		this.kunde = kundeid;
+	}
 
 //    public List<UserRoles> getUserRolesList() {
 //        return userRolesList;
@@ -101,31 +105,32 @@ public class Users2 implements Serializable {
 //    public void setUserRolesList(List<UserRoles> userRolesList) {
 //        this.userRolesList = userRolesList;
 //    }    
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (userId != null ? userId.hashCode() : 0);
-        return hash;
-    }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Users)) {
-            return false;
-        }
-        Users2 other = (Users2) object;
-        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (userId != null ? userId.hashCode() : 0);
+		return hash;
+	}
 
-    @Override
-    public String toString() {
-        return "wbs.corejpa.persistence.Users[ userId=" + userId + " ]";
-    }
-    
-    
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not
+		// set
+		if (!(object instanceof Users)) {
+			return false;
+		}
+		Users2 other = (Users2) object;
+		if ((this.userId == null && other.userId != null)
+				|| (this.userId != null && !this.userId.equals(other.userId))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "wbs.corejpa.persistence.Users[ userId=" + userId + " ]";
+	}
+
 }
